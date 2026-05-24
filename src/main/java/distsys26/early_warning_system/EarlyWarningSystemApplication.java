f328deb8012aedee42125d357ef6670677185dd7
Tran Bao <10423111@student.vgu.edu.vn>
2026-05-24 20:26:20 +0700

    Java, delete python

---
+++ src/main/java/distsys26/early_warning_system/EarlyWarningSystemApplication.java
@@ -0,0 +1,13 @@
+package distsys26.early_warning_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarlyWarningSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarlyWarningSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/distsys26/early_warning_system/EarlyWarningSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package distsys26.early_warning_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarlyWarningSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
